ee2a4619d166df7abfa5b4c6ee535c4481781c23
Devarence <32297872+Devarence@users.noreply.github.com>
2018-05-29 09:26:21 +0000

    Initial commit

---
+++ php/nav.php
@@ -0,0 +1,251 @@
+  <!-- nav -->          
+      <nav class="nav-primary hidden-xs">
+        <div class="text-muted text-sm hidden-nav-xs padder m-t-sm m-b-sm"><br/></div>
+        <ul class="nav nav-main" data-ride="collapse">
+          <li>
+            <a href="../home/dashboard1.php" class="auto">
+              <span class="pull-right text-muted">
+                <i class="i i-circle-sm-o text"></i>
+                <i class="i i-circle-sm text-active"></i>
+              </span>
+              <i class="i i-gauge icon">
+              </i>
+              <span class="font-bold">Tableau de bord</span>
+            </a>
+          </li>
+
+          <li >
+            <a href="#" class="auto">
+              <span class="pull-right text-muted">
+                <i class="i i-circle-sm-o text"></i>
+                <i class="i i-circle-sm text-active"></i>
+              </span>
+              <i class="i i-stack">
+              </i>
+              <span class="font-bold">Type Chambre</span>
+            </a>
+            <ul class="nav dk">
+              
+              <li class="active">
+                <ul class="nav dker">
+                  <li class="menu_moi">
+                    <a href="../typechambre/add.php">                                
+                      <span>Creer</span>
+                    </a>
+                  </li>
+                  <li class="menu_moi">
+                    <a href="../typechambre/dashboard.php">                                
+                      <span>Liste</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+
+          <li >
+            <a href="#" class="auto">
+              <span class="pull-right text-muted">
+                <i class="i i-circle-sm-o text"></i>
+                <i class="i i-circle-sm text-active"></i>
+              </span>
+              <i class="i i-stack">
+              </i>
+              <span class="font-bold">Chambre</span>
+            </a>
+            <ul class="nav dk">
+              
+              <li class="active">
+                <ul class="nav dker">
+                  <li class="menu_moi">
+                    <a href="../chambre/add.php">                                
+                      <span>Creer</span>
+                    </a>
+                  </li>
+                  <li class="menu_moi">
+                    <a href="../chambre/dashboard.php">                                
+                      <span>Liste</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+
+          <li >
+            <a href="#" class="auto">
+              <span class="pull-right text-muted">
+                <i class="i i-circle-sm-o text"></i>
+                <i class="i i-circle-sm text-active"></i>
+              </span>
+              <i class="i i-user2">
+              </i>
+              <span class="font-bold">Reservation</span>
+            </a>
+            <ul class="nav dk">
+              
+              <li class="active">
+                <ul class="nav dker">
+                  <li class="menu_moi">
+                    <a href="../reservation/add.php">                                
+                      <span>Creer</span>
+                    </a>
+                  </li>
+                  <li class="menu_moi">
+                    <a href="../reservation/dashboard.php">                                
+                      <span>Liste</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+
+          <li >
+            <a href="#" class="auto">
+              <span class="pull-right text-muted">
+                <i class="i i-circle-sm-o text"></i>
+                <i class="i i-circle-sm text-active"></i>
+              </span>
+              <i class="i i-users2">
+              </i>
+              <span class="font-bold">Client</span>
+            </a>
+            <ul class="nav dk">
+              
+              <li  class="active">
+                <ul class="nav dker">
+                  <li class="menu_moi">
+                    <a href="../client/add.php">                                
+                      <span>Creer</span>
+                    </a>
+                  </li>
+                  <li class="menu_moi">
+                    <a href="../client/dashboard.php">                                
+                      <span>Liste</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          
+          <li >
+            <a href="#" class="auto">
+              <span class="pull-right text-muted">
+                <i class="i i-circle-sm-o text"></i>
+                <i class="i i-circle-sm text-active"></i>
+              </span>
+              <i class="i i-stack">
+              </i>
+              <span class="font-bold">Enregistrement</span>
+            </a>
+            <ul class="nav dk">
+              
+              <li class="active">
+                <ul class="nav dker">
+                  <li class="menu_moi">
+                    <a href="../enregistrement/add.php">                                
+                      <span>Creer</span>
+                    </a>
+                  </li>
+                  <li class="menu_moi">
+                    <a href="../enregistrement/dashboard.php">                                
+                      <span>Liste</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+
+          <li >
+            <a href="#" class="auto">
+              <span class="pull-right text-muted">
+                <i class="i i-circle-sm-o text"></i>
+                <i class="i i-circle-sm text-active"></i>
+              </span>
+              <i class="i i-stack">
+              </i>
+              <span class="font-bold">Maintenance</span>
+            </a>
+            <ul class="nav dk">
+              
+              <li class="active">
+                <ul class="nav dker">
+                  <li class="menu_moi">
+                    <a href="../maintenance/add.php">                                
+                      <span>Creer</span>
+                    </a>
+                  </li>
+                  <li class="menu_moi">
+                    <a href="../maintenance/dashboard.php">                                
+                      <span>Liste</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+
+          <li >
+            <a href="#" class="auto">
+              <span class="pull-right text-muted">
+                <i class="i i-circle-sm-o text"></i>
+                <i class="i i-circle-sm text-active"></i>
+              </span>
+              <i class="i i-stack">
+              </i>
+              <span class="font-bold">Decaissement</span>
+            </a>
+            <ul class="nav dk">
+              
+              <li class="active">
+                <ul class="nav dker">
+                  <li class="menu_moi">
+                    <a href="../decaissement/add.php">                                
+                      <span>Creer</span>
+                    </a>
+                  </li>
+                  <li class="menu_moi">
+                    <a href="../decaissement/dashboard.php">                                
+                      <span>Liste</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+
+          <li >
+            <a href="#" class="auto">
+              <span class="pull-right text-muted">
+                <i class="i i-circle-sm-o text"></i>
+                <i class="i i-circle-sm text-active"></i>
+              </span>
+              <i class="i i-study">
+              </i>
+              <span class="font-bold">Bilan</span>
+            </a>
+            <ul class="nav dk">
+              
+              <li class="active">
+                <ul class="nav dker">
+                  <li class="menu_moi">
+                    <a href="../bilan/journalier.php">                                
+                      <span>Journalier</span>
+                    </a>
+                  </li>
+                  <li class="menu_moi">
+                    <a href="../bilan/perso.php">                                
+                      <span>Personnalisé</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+
+      </ul>
+      </nav>
+      <!-- / nav -->
